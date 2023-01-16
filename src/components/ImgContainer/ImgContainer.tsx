d8393110f05cb344c1c982e2137dafe38da3fc38
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2023-01-16 14:45:17 -0300

    new build

--- src/components/ImgContainer/ImgContainer.tsx
+++ src/components/ImgContainer/ImgContainer.tsx
@@ -1,9 +1,11 @@
 import libraryPhoto from './libraryPhoto.jpeg';
 import './ImgContainer.css';
+import Spacer from '../Spacer/Spacer';
 
 const ImgContainer = () => {
   return (
     <>
+      <Spacer height={20}/>
       <img src={libraryPhoto} className="Library-Photo" alt="libraryPhoto" />
     </>
   );
